ffe47ae6d3299e4369c42114158182868426f29e
Mario Kukutsov <kukucov@gmail.com>
2022-05-30 22:26:15 +0300

    Automate boring games so that you don't need to be bored while playing boring games. Why play boring games is a whole different topic.

--- README.md
+++ README.md
@@ -17,3 +17,14 @@ python3 console_quiz
 ```
 
 Nearly the entire logic can be done by a single class. The only method required to handle special cases is ```_correct()```. The taken approach is to define different subclasses implementing a separate the method as they see fit. It could be handled with a control structure within the method itself. But where is the fun in that?
+
+## infinite_cosmos
+
+On kongregate I "play" (play in quotes because it can hardly be called gameplay) an idle / upgrades garbo game called [Infinite Cosmos](https://www.kongregate.com/games/GoldhurstGames/infinite-cosmos). It wastes my time and I want to automate it.
+
+Run it with:
+```bash
+python infinite_cosmos
+```
+
+... Some more doc needed here
---
+++ infinite_cosmos/__main__.py
@@ -0,0 +1,109 @@
+import signal
+import pyautogui
+
+from time import sleep
+from typing import List, Tuple
+
+IMG_PATH: str = "infinite_cosmos/img/"
+
+BREAKTHROUGH_IMAGE_PATH: str = IMG_PATH + "breakthrough.png"
+UPGRADE_IMAGE_PATH: str = IMG_PATH + "upgrade.png"
+UPDATE_IMAGE_PATH: str = IMG_PATH + "update.png"
+HOLO_BLUE_A_IMAGE_PATH: str = IMG_PATH + "holo_blue_a.png"
+HOLO_BLUE_B_IMAGE_PATH: str = IMG_PATH + "holo_blue_b.png"
+
+
+class GameAutomator:
+    class UpgTrgt:
+        def __init__(self, x: int, y: int) -> None:
+            self.x = x
+            self.y = y
+
+        def click(self) -> None:
+            pyautogui.click(self.x, self.y)
+
+    def __init__(self) -> None:
+        # Handle KeyboardInterupt
+        self.running: bool = True
+        signal.signal(signal.SIGINT, self._sigint_handler)
+
+        # Create game screen rect
+        _: str = input("Place mouse at top left end of game screen")
+        self.left, self.top = pyautogui.position()
+
+        _: str = input("Place mouse at bottom right end of game screen")
+        self.right, self.bottom = pyautogui.position()
+
+        self.width: int = self.right - self.left
+        self.height: int = self.bottom - self.top
+
+        self.game_rect: Tuple[int] = (self.left, self.top, self.width, self.height)
+
+        # self.upgrades: List[self.UpgTrgt] = list()
+        self.set_upgrades()
+
+    def set_upgrades(self) -> None:
+        self.upgrades: List[self.UpgTrgt] = [
+            self.UpgTrgt(*pyautogui.center(ut))
+            for ut in pyautogui.locateAllOnScreen(
+                UPGRADE_IMAGE_PATH,
+                grayscale=False,
+                confidence=0.9,
+                region=self.game_rect,
+            )
+        ]
+
+    def breakthrough(self) -> None:
+        breakthrough = pyautogui.locateCenterOnScreen(
+            BREAKTHROUGH_IMAGE_PATH,
+            grayscale=False,
+            confidence=0.6,
+            region=self.game_rect,
+        )
+        if not breakthrough == None:
+            pyautogui.click(*breakthrough, clicks=300, interval=0.05)
+
+    def upgrade(self) -> None:
+        for up in self.upgrades:
+            up.click()
+
+    def holo_blue(self) -> None:
+        holo_blue_a = pyautogui.locateCenterOnScreen(
+            HOLO_BLUE_A_IMAGE_PATH,
+            grayscale=True,
+            confidence=0.6,
+            region=self.game_rect,
+        )
+        holo_blue_b = pyautogui.locateCenterOnScreen(
+            HOLO_BLUE_B_IMAGE_PATH,
+            grayscale=True,
+            confidence=0.6,
+            region=self.game_rect,
+        )
+        print(f"HB A: {holo_blue_a} HB B: {holo_blue_b}")
+        if not holo_blue_a == None:
+            pyautogui.click(*holo_blue_a)
+        if not holo_blue_b == None:
+            pyautogui.click(*holo_blue_b)
+
+    def play(self) -> None:
+        while self.running:
+            init_x, init_y = pyautogui.position()
+
+            self.holo_blue()
+            self.breakthrough()
+            self.upgrade()
+
+            pyautogui.moveTo(self.top, self.left)
+            sleep(1)
+            pyautogui.moveTo(init_x, init_y)
+            sleep(5)
+
+    def _sigint_handler(self, signal, frame) -> None:
+        print(signal, frame)
+        self.running = False
+
+
+if __name__ == "__main__":
+    ga: GameAutomator = GameAutomator()
+    ga.play()
